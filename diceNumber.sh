f3b767a4e3087aa5dd512f4b4f297d6ad503809c
Arunsh93 <Arunhosur93@gmail.com>
2021-09-18 16:57:16 +0530

    Roll Dice Number

--- diceNumber.sh
+++ diceNumber.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 declare -A diceNum
 diceNum[1]=0
@@ -8,38 +8,36 @@ diceNum[4]=0
 diceNum[5]=0
 diceNum[6]=0
 
-for (( i=0; i<100; i++ ))
-do 
-ranCheck=$((RANDOM%6+1))
-echo "$ranCheck"
-diceNum[$ranCheck]=$(( ${diceNum[$ranCheck]} + 1 ))
+for (( i=0; i< 100; i++ ))
+do
+ranChk=$(( RANDOM % 6 + 1 ))
+echo "$ranChk"
+diceNum[$ranChk]=$(( ${diceNum[$ranChk]} + 1 ))
 
-	if [ ${diceNum[$ranCheck]} == 10 ]
-	then
-		echo "-----------------------"
-	break
-	fi
+   if [ ${diceNum[$ranChk]} == 10 ]
+   then
+   echo "---------------------"
+   break
+   fi
 done
-
-echo "dice count is ${!diceNum[@]} is :::: ${diceNum[@]}"
-echo "----------------------------"
-echo "$ranCheck :: wins first"
-echo "----------------------------"
-echo "----------------------------"
+echo "dice counts is ${!diceNum[@]} is :::: ${diceNum[@]}"
+echo "---------------------"
+echo "$ranChk :: wins first"
+echo "---------------------"
+echo "---------------------"
 diceMin=${diceNum[1]}
 diceMax=${diceNum[1]}
 
 for i in ${diceNum[@]}
 do
-	if [ $i -lt $diceMin ]
-	then
-		diceMin=$i
-	fi
-	if [ $i -gt $diceMax ]
-	then
-		diceMax=$i
-	fi
+   if [ $i -lt $diceMin ]
+   then
+      diceMin=$i
+   fi
+   if [ $i -gt $diceMax ]
+   then
+      diceMax=$i
+   fi
 done
-	
-echo "Maximum Numbers ::$diceMax"
-echo "Minimum numbers ::$diceMin"
+echo "Maximum numbers ::$diceMax"
+echo "Minimum mumbers ::$diceMin"
